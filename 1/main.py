d6af003eeaa59ecf673be7dfaf0f2d06ce0b1fb1
Jankowski Łukasz <73948593+aquadros1003@users.noreply.github.com>
2021-03-18 12:44:54 +0100

    cw1

--- 1/main.py
+++ 1/main.py
@@ -1,3 +1,4 @@
+from math import *
 #zad1
 a1 = "zadanie na wd"
 b1 = "zadanie ma wizualizacje danych"
@@ -9,17 +10,26 @@ g1 = 3 + 4j
 h1 = 5 + 7j
 print(a1,b1,c1,d1,e1,f1,g1,h1)
 #zad2
-a2 = 7
-b2 = 5
-suma2 = a2 + b2
-roznica2 = a2 - b2
-iloczyn2 = a2 * b2
-iloraz2 = a2 / b2
-print(suma2,roznica2,iloczyn2,iloraz2)
+pierwsza_liczba = input("Podaj pierwszą liczbę: ")
+znak = input("Podaj znak działania: ")
+druga_liczba = input("Podaj drugą liczbę: ")
+if znak == "+":
+    suma = int(pierwsza_liczba) + int(druga_liczba)
+    print(suma)
+elif znak == "-":
+    roznica = int(pierwsza_liczba) - int(druga_liczba)
+    print(roznica)
+elif znak == "*":
+    iloczyn = int(pierwsza_liczba) * int(druga_liczba)
+    print(iloczyn)
+elif znak == "/":
+    iloraz = int(pierwsza_liczba) / int(druga_liczba)
+    print(iloraz)
+else:
+    print("error")
 #zad3
 print(suma2 + roznica2,iloczyn2 - suma2,suma2 * roznica2, iloraz2 / iloczyn2,suma2 ** roznica2,iloczyn2 % roznica2)
 #zad4
-from cmath import *
 print(exp(10),sqrt(log(5+sin(8)**2,6)),[3,55],[4,80])
 #zad5
 imie = 'LUKASZ'
@@ -35,7 +45,10 @@ print(lancuch[1],lancuch[-1])
 x8 = piosenka.split()
 print(x8)
 #zad9
-a = 'wd'
-b = 5.7
-c = 57
-print(str(a),float(b),hex(c))
+napis2 = "Jest dziś poniedziałek"
+print("Jaki mamy dziś dzień? %s " %  (napis2))
+liczba = 5.677
+print("twoja liczba to: %(z1).2f" % {'z1': liczba})
+liczba_szesnastkowo = 0xf351
+print(liczba_szesnastkowo)
+print("%x" % (liczba_szesnastkowo))
