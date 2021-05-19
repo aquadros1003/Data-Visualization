8d196282185f8508bc01f64b2efbf8d1de13e856
Jankowski Łukasz <73948593+aquadros1003@users.noreply.github.com>
2021-05-19 18:09:23 +0200

    cwiczenia 11

--- cwiczenia/cwiczenia11/main.py
+++ cwiczenia/cwiczenia11/main.py
@@ -1,97 +1,80 @@
 import numpy as np
+import math as m
 import pandas as pd
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
-import openpyxl
-from pathlib import Path
 
-# ts = pd.Series(np.random.randn(1000), index = pd.date_range('1/1/2015',periods=1000))
-#
-# ts = ts.cumsum()
-# print(ts)
-# ts.plot()
-# plt.show()
 
-# data = {'Kraj': ['Belgia', 'Indie', 'Brazylia','Polska'],
-#         'Stolica': ['Bruksela', 'New Delhi', 'Brasilia', 'Warszawa'],
-#         'Kontynent': ['Europa', 'Azja', 'Ameryka Południowa', 'Europa'],
-#         'Populacja': [11190846, 13031711035, 38676567, 38675467]}
-#
-# df = pd.DataFrame(data)
-# print(df)
-#
-# grupa = df.groupby(['Kontynent']).agg({'Populacja': ['sum']})
-# print(grupa)
-#
-# wykres = grupa.plot.bar()
-# wykres.set_xlabel('Kontynent')
-# wykres.set_ylabel('Mld')
-# wykres.legend()
-# plt.xticks(rotation = 0)
-# plt.savefig('wykres.png')
-# plt.title('Populacja z podzialem na kontynenty')
+#ZADANIE 1
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# t = np.linspace(0, 2 * np.pi, 100)
+# z = t
+# x = np.sin(t)
+# y = 2 * np.cos(t)
+# ax.plot(x, y, z)
 # plt.show()
 
-# df = pd.read_csv('dane.csv',header = 0, sep = ';', decimal = '.')
-# print (df)
-# grupa = df.groupby(['Imię i nazwisko']).agg({'Wartość zamówienia':['sum']})
-# print(grupa)
-# wykres = grupa.plot.pie(subplots = True, autopct = '%.2f %%', fontsize = 20, figsize = (6, 6), legend = (0,0))
-# plt.legend()
-# plt.legend(loc = 'lower right')
-# plt.title('Suma zamówienia dla sprzedawcy')
+#ZADANIE 2
+# np.random.seed(19680801)
+# def randrange(n, vmin, vmax):
+#     return (vmax - vmin) * np.random.rand(n) + vmin
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# x1 = randrange(10, 32, 32)
+# y1 = randrange(10, 32, 42)
+# z1 = randrange(10, 46, 45)
+# x2 = randrange(10, 42, 72)
+# y2 = randrange(10, 13, 3)
+# z2 = randrange(10, 45, 131)
+# x3 = randrange(10, 13, 53)
+# y3 = randrange(10, 51, 14)
+# z3 = randrange(10, 53, 53)
+# x4 = randrange(10, 23, 15)
+# y4 = randrange(10, 76, 34)
+# z4 = randrange(10, 23, 151)
+# x5 = randrange(10, 41, 151)
+# y5 = randrange(10, 42, 105)
+# z5 = randrange(10, 86, 53)
+# ax.scatter(x1, y1, z1, c='b', marker='X')
+# ax.scatter(x2, y2, z2, c='r', marker='p')
+# ax.scatter(x3, y3, z3, c='c', marker='*')
+# ax.scatter(x4, y4, z4, c='k', marker='+')
+# ax.scatter(x5, y5, z5, c='y', marker='o')
 # plt.show()
 
-# ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2015', periods=1000))
-#
-# ts = ts.cumsum()
-#
-# df = pd.DataFrame(ts)
-#
-# df['MA'] = df.rolling(window=50).mean()
-# df.plot()
+#ZADANIE 4
+# fig = plt.figure(figsize=(12, 10))
+# ax1 = fig.add_subplot(1, 5, 1, projection='3d')
+# ax2 = fig.add_subplot(1, 5, 2, projection='3d')
+# ax3 = fig.add_subplot(1, 5, 3, projection='3d')
+# ax4 = fig.add_subplot(1, 5, 4, projection='3d')
+# ax5 = fig.add_subplot(1, 5, 5, projection='3d')
+# _x = np.arange(4)
+# _y = np.arange(5)
+# _xx, _yy = np.meshgrid(_x, _y)
+# x, y = _xx.ravel(), _yy.ravel()
+# top = x + y
+# bottom = np.zeros_like(top)
+# width = depth = 1
+# ax1.bar3d(x, y, bottom, width, depth, top, color='g')
+# ax2.bar3d(x, y, bottom, width, depth, top, color='k', shade=False)
+# ax3.bar3d(x, y, bottom, width, depth, top, color='c')
+# ax4.bar3d(x, y, bottom, width, depth, top, color='y', )
+# ax5.bar3d(x, y, bottom, width, depth, top, color='b', shade=True)
 # plt.show()
 
-# #ZADANIA
-# #1
-# xlsx = pd.ExcelFile('imiona.xlsx')
-# df = pd.read_excel(xlsx, header=0)
-# grupa1 = df.groupby(['Rok']).agg({'Liczba': ['sum']})
-# wykres1 = grupa1.plot()
-# wykres1.set_ylabel("Liczba urodzeń")
-# wykres1.set_xlabel("Rok")
-# wykres1.legend()
-# plt.title("Liczba urodzonych dzieci w poszczególnych latach")
-# plt.show()
-# #2
-# xlsx = pd.ExcelFile('imiona.xlsx')
-# df = pd.read_excel(xlsx, header=0)
-#
-# grupa2 = df.groupby(['Plec']).agg({'Liczba': ['sum']})
-# wykres2 = grupa2.plot.bar()
-# wykres2.set_ylabel("Liczba urodzeń")
-# wykres2.set_xlabel("Płeć")
-# wykres2.legend()
-# plt.title("Liczba urodzonych dzieci ze względu na płeć")
-# plt.xticks(rotation=0)
-# plt.show()
-#
-# #3
-# xlsx = pd.ExcelFile('imiona.xlsx')
-# df = pd.read_excel(xlsx, header=0)
-# df = df[(df['Rok'] >= 2013) & (df['Rok'] <= 2017)]
-# grupa3 = df.groupby(['Plec']).agg({'Liczba': ['sum']})
-# wykres3 = grupa3.plot.pie(subplots=True, autopct='%.2f %%', fontsize=20, figsize=(6, 6), legend=(0, 0))
-# plt.legend(loc="lower right")
-# plt.title("Stosunek urodzeń ze względu na płeć")
-# plt.show()
-#
-# #4
-# df = pd.read_csv('zamowienia.csv', header=0, sep=';', decimal=',')
-# grupa4 = df.groupby(['Sprzedawca']).agg({'idZamowienia': ['count']})
-# wykres4 = grupa4.plot.bar()
-# wykres4.set_ylabel("Ilosc zamowien")
-# wykres4.set_xlabel("Sprzedawcy")
-# wykres4.legend()
-# plt.title("Ilość zamówień na poszczegolnych sprzedawcow")
-# plt.xticks(rotation=0)
+#ZADANIE 3
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# X = np.arange(-5, 5, 0.25)
+# Y = np.arange(-5, 5, 0.25)
+# X, Y = np.meshgrid(X, Y)
+# R = np.sqrt(X**2 + Y**2)
+# Z = np.sin(R)
+# surface = ax.plot_surface(X, Y, Z, cmap=plt.get_cmap(), linewidth=0, antialiased=True)
+# ax.set_zlim(-1.01, 1.01)
+# ax.zaxis.set_major_locator(LinearLocator(10))
+# ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+# fig.colorbar(surface, shrink=1, aspect=5)
 # plt.show()
